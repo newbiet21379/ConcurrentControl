8f287a29dd958e95f8e9b8248df3e92d1adc0959
brendanpham22 <101374957+brendanpham22@users.noreply.github.com>
2024-03-17 23:54:29 +0700

    Init Split

---
+++ transaction-case/src/test/java/com/tim/transactioncase/TestTransactionCaseApplication.java
@@ -0,0 +1,23 @@
+package com.tim.transactioncase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestTransactionCaseApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(TransactionCaseApplication::main).with(TestTransactionCaseApplication.class).run(args);
+    }
+
+}
---
+++ transaction-case/src/test/java/com/tim/transactioncase/TransactionCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim.transactioncase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
